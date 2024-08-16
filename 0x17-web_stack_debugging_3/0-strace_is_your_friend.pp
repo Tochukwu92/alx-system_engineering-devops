1a6f881e8089d1710e452c7d172a7d4f190445a1
Tochukwu92 <Ilonubatochukwu@gmail.com>
2024-08-16 02:52:09 +0100

    webstack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,3 +3,18 @@
 exec{'fix-wordpress':
 	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 	path    => '/usr/local/bin/:/bin/'
+
+#  Fix permission for the web directory
+
+file { '/var/www/html':
+	ensure	=> directory,
+	owner	=> 'www-data',
+	group	=> 'www-data',
+	mode	=> '0755',
+	recurse	=> true,
+
+#  Ensure Apache is running
+
+service { 'apache2':
+	ensure	=> running,
+	enable	=> true,
